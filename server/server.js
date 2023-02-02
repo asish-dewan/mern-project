de3850c931784e2a4185b1c41fe8752655e9b937
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-02-02 19:45:32 +1100

    Fixed mongo connection error

--- server/server.js
+++ server/server.js
@@ -1,27 +1,19 @@
 
 const express = require('express');
-const cors = require('cors');
-const multer = require('multer');
-const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('./config/connection');
 const routes = require('./routes');
 const dotenv = require('dotenv');
-
-const register = require("./utils/auth.js").register();
 
 /* CONFIGURATIONS */
 
 dotenv.config();
+const db = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 8001;
+console.log(process.env.MONGO_URL)
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use (cors());
-// app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -30,27 +22,16 @@ if (process.env.NODE_ENV === 'production') {
 
 /* FILE STORAGE */
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
-});
-
-const upload = multer({ storage });
-
 /* ROUTE WITH FILES */
 
 /* Upload sample locally to the public/assets folder before hitting the register endpoint 
     This route cannot be moved to Routes as we require the 'upload' variable */
 
-app.post ("/auth/register", upload.single("sample"), register);
+//app.post ("/auth/register", upload.single("sample"), createUser();
 
 /* ROUTES */
 
-app.use ("/auth", routes);
+app.use (routes);
 
 db.once('open', () => {
     app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
