5dfcdb583368c2fb1af555481f6b1a381940b250
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-02-03 01:20:38 +1100

    Updated signToken function parameters + refactored token secret

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,16 +1,19 @@
 const jwt = require('jsonwebtoken');
 
+// Token Secret
+const secret = process.env.JWT_SECRET;
 // set expiration date 
-
 const expiration = '1h';
 
 module.exports = {
     authMiddleware: function (req, res, next) {
         // allows token to be sent via req.query or headers
-        let token = req.query.token || req.headers.authorization;
+        let token = req.body.token || req.query.token || req.headers.authorization;
 
         if (req.headers.authorization) {
-            token = token.split(' ').pop().trim();
+            token = token.split(' ')
+            .pop()
+            .trim();
         }
 
         if (!token) {
@@ -19,7 +22,7 @@ module.exports = {
 
         // Verify token and get user data
         try {
-            const { data } = jwt.verify(token, process.env.JWT_SECRET, { maxAge: expiration });
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
             req.user = data;
         } catch {
             console.log ('Invalid Token');
@@ -30,9 +33,9 @@ module.exports = {
         next();
     },
 
-    signToken: function ({ email, password }) {
-        const payload = { email, password };
+    signToken: function ({ username, email, _id }) {
+        const payload = { username, email, _id };
 
-        return jwt.sign({ data: payload}, process.env.JWT_SECRET, { expiresIn: expiration });
+        return jwt.sign({ data: payload}, secret, { expiresIn: expiration });
     },
 };
