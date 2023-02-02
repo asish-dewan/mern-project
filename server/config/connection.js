e97411fb187321a68bc1899ef59e2bc3152b9476
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-02-02 19:55:31 +1100

    Removed mongoose userCreateIndex and userFindAndModify - this was causing an error when trying to run the server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,11 @@
 const mongoose = require ('mongoose');
+const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/tyro'
 
 /* MONGOOSE SETUP */
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-
-})
+});
 
 module.exports = mongoose.connection;
