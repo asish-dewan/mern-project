09668ab2194cf5fb10569c275d99c150c394c410
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-02-04 02:50:51 +1100

    Added local mongo server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require ('mongoose');
-const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/tyro'
+const url = /*process.env.MONGO_URL */ 
 
 /* MONGOOSE SETUP */
 
-mongoose.connect(url, {
+mongoose.connect(url || 'mongodb://127.0.0.1:27017/tyro-sounds', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
