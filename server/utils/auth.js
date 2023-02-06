c438b34cd21357982c058be71d906c1a3225a71c
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-02-06 18:24:20 +1100

    Updated authentication error

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,13 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 // Token Secret
-const secret = process.env.JWT_SECRET;
+const secret = 'cr7>ms!';
 // set expiration date 
 const expiration = '1h';
 
 module.exports = {
-    authMiddleware: function (req, res, next) {
+    authMiddleware: function ({req}, res, next) {
         // allows token to be sent via req.query or headers
+        console.log(req.body.token)
+        console.log(req.query.token)
+        console.log(req.headers.authorization)
+        
         let token = req.body.token || req.query.token || req.headers.authorization;
 
         if (req.headers.authorization) {
@@ -17,7 +21,8 @@ module.exports = {
         }
 
         if (!token) {
-            return res.status(400).json({ message: 'You have no token!'});
+            return req;
+            //return res.status(400).json({ message: 'You have no token!'});
         }
 
         // Verify token and get user data
@@ -26,11 +31,11 @@ module.exports = {
             req.user = data;
         } catch {
             console.log ('Invalid Token');
-            return res.status(400).json ({ message: 'Invalid Token!'});
+            //return res.status(400).json ({ message: 'Invalid Token!'});
         }
 
         // Direct to next endpoint 
-        next();
+        return req;
     },
 
     signToken: function ({ username, email, _id }) {
