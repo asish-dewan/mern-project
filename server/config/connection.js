5711a4c2137b1f43628533c6955141e8958a027f
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-02-07 18:54:29 +1100

    Refactored code and commented out remote server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require ('mongoose');
-const url = process.env.MONGO_URL
 
 /* MONGOOSE SETUP */
 
-mongoose.connect('mongodb://127.0.0.1:27017/tyro-sounds', {
+mongoose.connect(/*process.env.MONGO_URI ||*/ 'mongodb://127.0.0.1:27017/tyro-sounds', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
