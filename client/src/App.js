fd3b28b1d833e8ebbce84fbbace6d710157e6377
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-02-07 18:59:44 +1100

    Updated proxy port, Added tailwind config files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import './App.css';
-
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -14,17 +12,21 @@ import SinglePost from './pages/SinglePost';
 import Profile from './pages/Profile';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: '/graphql'
+
 });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token');
   // return the headers to the context so httpLink can read them
+
+console.log(`tokennn`,token);
+
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
